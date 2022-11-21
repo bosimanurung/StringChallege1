c84d0d80826f7b0c3b914f48ebf5b042c438e061
Bosi Manurung <94754059+bosimanurung@users.noreply.github.com>
2022-11-21 17:35:26 +0700

    add description at the top of program

--- StringChallenge.py
+++ StringChallenge.py
@@ -3,6 +3,10 @@
 Created on Wed Jul 27 23:38:49 2022
 
 @author: bosimanurung
+
+Have the function DifferentCases(str) take the str parameter being passed and return it in upper camel case format 
+where the first letter of each word is capitalized. The string will only contain letters and some combination of delimeter punctuation character 
+separating each word.
 """
 
 def StringChallenge(strParam):
